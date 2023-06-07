09ea9a56ba0e610f6039b7678123c4478d039624
Jinju Narimatsu <jinju.narimatsu@hotmail.co.jp>
2023-06-07 18:09:10 +1000

    fully functioning commit

--- README.md
+++ README.md
@@ -5,3 +5,38 @@ The configuration could include information about what screen dimensions and res
 
 Deployment procedures is referring to any steps we need to complete in order to run your web application. For example, do we need to run 'npm install' to download the dependencies. Then which command should we use to start the server, likely either 'npm run dev' or 'npm run start'.
 
+
+_________DEPLOYMENT PROCEDURES_________
+1.'npm express install'
+2.'npm run start'
+
+_________CONFIGURATION_________
+The following screen dimensions are best for viewing this application.
+Web: 1440px × 900px
+Mobile: 489px x 926px
+
+____Sample Form Inputs____
+You can write in any book details you would like. But here are some sample inputs to get you started. 
+-Title (The name of the book): The Great Gatsby
+-Author (The full name of the author): F. Scott Fitzgerald
+-Category (The category of this genre): "Classics"
+-Image (A .png of the book cover): https://imgur.com/lwPwrOx (You will need to download this image to upload)
+-Review: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+
+
+
+
+
+_________THE DEVELOPMENT PROCESS:_________
+-Decided to scrap the menu on the navigation bar as I felt like it was unnecessary when everything was on one page anyway.
+-Decided to add some placeholder text in the form for an easier user experience.  
+
+-I found it challenging to make the booklist table be responsive for very small screens. The only way I can think of making the table work on a small screen (without splitting the structure) was to make users scroll horizontally. 
+C
+-I struggled to put an image for the book cover for the sample Book Data ;-;
+
+_________MOVING FORWARDS- Recommentations and Improvements:_________
+-Consider integrating book API's so that users do not need to manually input a book cover. 
+
+
--- public/script.js
+++ public/script.js
@@ -1,131 +1,187 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById('bookForm');
-  const bookTableBody = document.getElementById('bookTableBody');
-  const bookData = [];
-
-  form.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const bookNameInput = document.getElementById('bookName');
-    const authorNameInput = document.getElementById('authorName');
-    const bookCategoryInput = document.getElementById('bookCategory');
-    const bookRatingInput = document.getElementById('bookRating');
-    const bookReviewInput = document.getElementById('bookReview');
-    const bookImageInput = document.getElementById('bookImage');
-
-    const bookName = bookNameInput.value;
-    const authorName = authorNameInput.value;
-    const bookCategory = bookCategoryInput.value;
-    const bookRating = parseInt(bookRatingInput.value);
-    const bookReview = bookReviewInput.value;
-    const bookImage = bookImageInput.files[0];
-
-    bookNameInput.value = '';
-    authorNameInput.value = '';
-    bookCategoryInput.value = '';
-    bookRatingInput.value = '';
-    bookReviewInput.value = '';
-    bookImageInput.value = '';
-
-    const currentDate = new Date();
-    const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
-
-    const book = {
-      bookName: bookName,
-      authorName: authorName,
-      bookCategory: bookCategory,
-      bookRating: bookRating,
-      date: formattedDate,
-      bookReview: bookReview,
-      bookImage: bookImage
-    };
-
-    bookData.push(book);
-
-    renderBookTable();
-  });
-
-  function renderBookTable() {
-    bookTableBody.innerHTML = '';
-
-    bookData.forEach(function (book, index) {
-      const row = document.createElement('tr');
-
-      const nameCell = document.createElement('td');
-      nameCell.textContent = book.bookName;
-      row.appendChild(nameCell);
-
-      const authorCell = document.createElement('td');
-      authorCell.textContent = book.authorName;
-      row.appendChild(authorCell);
-
-      const categoryCell = document.createElement('td');
-      categoryCell.textContent = book.bookCategory;
-      row.appendChild(categoryCell);
-
-      const ratingCell = document.createElement('td');
-      if (Number.isInteger(book.bookRating) && book.bookRating >= 1 && book.bookRating <= 5) {
-        const stars = '★'.repeat(book.bookRating) + '☆'.repeat(5 - book.bookRating);
-        ratingCell.textContent = stars;
-      } else {
-        ratingCell.textContent = 'Invalid Rating';
-      }
-      row.appendChild(ratingCell);
-
-      const dateCell = document.createElement('td');
-      dateCell.textContent = book.date;
-      row.appendChild(dateCell);
-
-      const readMoreCell = document.createElement('td');
-      const readMoreButton = document.createElement('button');
-      readMoreButton.textContent = 'Read More';
-      let isExpanded = false;
-
-      readMoreButton.addEventListener('click', function () {
-        if (!isExpanded) {
-          const expandedRow = document.createElement('tr');
-
-          const expandedCell = document.createElement('td');
-          expandedCell.colSpan = 6;
-
-          const reviewDiv = document.createElement('div');
-          reviewDiv.textContent = `Book Review: ${book.bookReview}`;
-          expandedCell.appendChild(reviewDiv);
-
-          if (book.bookImage) {
-            const image = document.createElement('img');
-            image.src = URL.createObjectURL(book.bookImage);
-            image.style.width = '200px';
-            expandedCell.appendChild(image);
-          }
-
-          expandedRow.appendChild(expandedCell);
-          row.insertAdjacentElement('afterend', expandedRow);
-        } else {
-          row.nextElementSibling.remove();
-        }
-
-        isExpanded = !isExpanded;
-      });
-
-      readMoreCell.appendChild(readMoreButton);
-      row.appendChild(readMoreCell);
-
-      const deleteCell = document.createElement('td');
-      const deleteButton = document.createElement('button');
-      deleteButton.innerHTML = '🗑';
-      deleteButton.addEventListener('click', function () {
-        deleteBook(index);
-      });
-      deleteCell.appendChild(deleteButton);
-      row.appendChild(deleteCell);
-
-      bookTableBody.appendChild(row);
+    const form = document.getElementById('bookForm'); // Get the book form element
+    const bookTableBody = document.getElementById('bookTableBody'); // Get the book table body element
+    const bookData = []; // Array to store book data
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault(); // Prevent form submission
+  
+      // Get input values from the form
+      const bookNameInput = document.getElementById('bookName');
+      const authorNameInput = document.getElementById('authorName');
+      const bookCategoryInput = document.getElementById('bookCategory');
+      const bookRatingInput = document.getElementById('bookRating');
+      const bookReviewInput = document.getElementById('bookReview');
+      const bookImageInput = document.getElementById('bookImage');
+  
+      // Extract values from input elements
+      const bookName = bookNameInput.value;
+      const authorName = authorNameInput.value;
+      const bookCategory = bookCategoryInput.value;
+      const bookRating = parseInt(bookRatingInput.value);
+      const bookReview = bookReviewInput.value;
+      const bookImage = bookImageInput.files[0];
+  
+      // Clear input fields
+      bookNameInput.value = '';
+      authorNameInput.value = '';
+      bookCategoryInput.value = '';
+      bookRatingInput.value = '';
+      bookReviewInput.value = '';
+      bookImageInput.value = '';
+  
+      // Get current date and format it
+      const currentDate = new Date();
+      const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
+  
+      // Create a book object with the extracted values
+      const book = {
+        bookName: bookName,
+        authorName: authorName,
+        bookCategory: bookCategory,
+        bookRating: bookRating,
+        date: formattedDate,
+        bookReview: bookReview,
+        bookImage: bookImage
+      };
+  
+      // Add the book to the bookData array
+      bookData.push(book);
+  
+      // Render the book table with the updated data
+      renderBookTable();
     });
-  }
-
-  function deleteBook(index) {
-    bookData.splice(index, 1);
+  
+    // Function to render the book table
+    function renderBookTable() {
+      bookTableBody.innerHTML = ''; // Clear the table body
+  
+      // Iterate over each book in the bookData array
+      bookData.forEach(function (book, index) {
+        const row = document.createElement('tr'); // Create a new table row
+  
+        // Create table cells for book information
+        const nameCell = document.createElement('td');
+        nameCell.textContent = book.bookName;
+        row.appendChild(nameCell);
+        nameCell.style.fontWeight = 'bolder'; // Make the name cell bold
+  
+        const authorCell = document.createElement('td');
+        authorCell.textContent = book.authorName;
+        row.appendChild(authorCell);
+  
+        const categoryCell = document.createElement('td');
+        categoryCell.textContent = book.bookCategory;
+        row.appendChild(categoryCell);
+  
+        const ratingCell = document.createElement('td');
+        if (Number.isInteger(book.bookRating) && book.bookRating >= 1 && book.bookRating <= 5) {
+          const stars = '★'.repeat(book.bookRating) + '☆'.repeat(5 - book.bookRating); // Display stars based on the rating
+          ratingCell.textContent = stars;
+        } else {
+          ratingCell.textContent = 'Invalid Rating'; // Display "Invalid Rating" for invalid ratings
+        }
+        row.appendChild(ratingCell);
+        ratingCell.style.color = "#FFF09E"; // Set the color of the rating cell
+  
+        const dateCell = document.createElement('td');
+        dateCell.textContent = book.date;
+        row.appendChild(dateCell);
+  
+        const readMoreCell = document.createElement('td');
+        const readMoreButton = document.createElement('button');
+        readMoreButton.textContent = 'Read';
+        let isExpanded = false;
+  
+        // Event listener for the "Read" button to expand/collapse the book review
+        readMoreButton.addEventListener('click', function () {
+          if (!isExpanded) {
+            const expandedRow = document.createElement('tr');
+  
+            const expandedCell = document.createElement('td');
+            expandedCell.colSpan = 6; // Span the expanded cell across all columns
+            expandedCell.style.flexDirection = 'column';
+            expandedCell.style.backgroundColor = '#DFD4C1';
+            expandedCell.style.padding = '20px';
+            expandedCell.style.width = 'auto';
+            expandedCell.style.borderRadius = '10px';
+            expandedCell.style.transform = '0.5s';
+  
+            // Create a div to display the book review
+            const reviewDiv = document.createElement('div');
+            reviewDiv.textContent = `Book Review: ${book.bookReview}`;
+            expandedCell.appendChild(reviewDiv);
+            reviewDiv.style.paddingBottom = '10px';
+  
+            // Display book image if available
+            if (book.bookImage) {
+              const image = document.createElement('img');
+              image.src = URL.createObjectURL(book.bookImage);
+              image.style.width = '150px';
+              image.style.height = 'auto';
+              expandedCell.appendChild(image);
+            }
+  
+            expandedRow.appendChild(expandedCell);
+            row.insertAdjacentElement('afterend', expandedRow);
+            readMoreButton.textContent = 'Collapse';
+          } else {
+            row.nextElementSibling.remove();
+            readMoreButton.textContent = 'Read';
+          }
+  
+          isExpanded = !isExpanded; // Toggle the expanded state
+        });
+  
+        readMoreCell.appendChild(readMoreButton);
+        row.appendChild(readMoreCell);
+  
+        const deleteCell = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'X';
+  
+        // Event listener for the "X" button to delete the book
+        deleteButton.addEventListener('click', function () {
+          deleteBook(index); // Call the deleteBook function with the index of the book
+        });
+        deleteCell.appendChild(deleteButton);
+        row.appendChild(deleteCell);
+  
+        bookTableBody.appendChild(row); // Append the row to the table body
+      });
+    }
+  
+    // Function to delete a book from the bookData array
+    function deleteBook(index) {
+      bookData.splice(index, 1); // Remove the book at the specified index
+      renderBookTable(); // Render the book table with the updated data
+    }
+    
+    // Sample Book Data
+    const initialBooks = [
+      {
+        bookName: "The Giver",
+        authorName: "Miguel de Cervantes",
+        bookCategory: "Fiction",
+        bookRating: 4,
+        date: "01/06/2023",
+        bookReview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        bookImage: null
+      },
+      {
+        bookName: "Jane Eyre",
+        authorName: "Charlotte Brontë",
+        bookCategory: "Classics",
+        bookRating: 3,
+        date: "02/06/2023",
+        bookReview: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        bookImage: null
+      },
+    ];
+  
+    // Add initial books to the bookData array
+    bookData.push(...initialBooks);
+  
+    // Render the book table with initial data
     renderBookTable();
-  }
 });
